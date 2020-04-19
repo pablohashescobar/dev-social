a1ace0b0cfb80a7d70418fe267825998f006457d
nikhil <nikhil13012000@gmail.com>
2020-04-19 15:47:14 +0530

    get user repo

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const auth = require("../../middleware/auth");
 const { check, validationResult } = require("express-validator");
 const normalize = require("normalize-url");
+const axios = require("axios");
+const config = require("config");
 
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
@@ -251,6 +253,115 @@ router.delete("/experience/:exp_id", auth, async (req, res) => {
   } catch (err) {
     console.error(err.message);
     res.status(400).send("Internal Serval Error");
+  }
+});
+
+//@route PUT api/profile/education
+//@desc  Put Experience
+//@acess Private
+
+router.put(
+  "/education",
+  [
+    auth,
+    [
+      check("school", "School is Required").not().isEmpty(),
+      check("degree", "Degree is Required").not().isEmpty(),
+      check("fieldofstudy", " Field of Study is Required").not().isEmpty(),
+      check("from", " From date is Required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+
+    const newEdu = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.education.unshift(newEdu);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
+//@route DELETE api/profile/education/:edu_id
+//@desc  Delete education
+//@acess Private
+
+router.delete("/education/:exp_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    //Get Remove Index
+    const removeIndex = profile.education
+      .map((item) => item.id)
+      .indexOf(req.params.edu_id);
+
+    profile.education.splice(removeIndex, 1);
+    console.log("Remove Index", removeIndex);
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(400).send("Internal Serval Error");
+  }
+});
+
+//@route GET api/profile/github/:username
+//@desc  GET User REpos from github
+//@acess Public
+
+router.get("/github/:username", async (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        "githubClientId"
+      )}&client_secret=${config.get("githubSecret")}`,
+    };
+
+    const gitHubResponse = await axios.get(options.uri, {
+      headers: { "user-agent": "node.js" },
+    });
+
+    if (gitHubResponse.status === !200) {
+      return res.status(404).json({ msg: "No user Found" });
+    }
+
+    return res.json(gitHubResponse.data);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("No User Found");
   }
 });
 
