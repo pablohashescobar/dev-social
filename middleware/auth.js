6ad567b1e0aa56d34432dea77ae16791b275603e
nikhil <nikhil13012000@gmail.com>
2020-04-26 10:57:08 +0530

    auth bug

--- middleware/auth.js
+++ middleware/auth.js
@@ -14,7 +14,7 @@ module.exports = function (req, res, next) {
   //Verify token
   try {
     const decoded = jwt.verify(token, config.get("jwtSecret"));
-    console.log(decoded);
+
     req.user = decoded.user;
 
     next();
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -124,6 +124,10 @@ router.put("/like/:id", auth, async (req, res) => {
       return res.status(400).json({ msg: "Post already liked" });
     }
 
+    if (req.user.id === post.user.toString()) {
+      return res.status(400).json({ msg: "Cannot like your own post" });
+    }
+
     post.likes.unshift({ user: req.user.id });
 
     await post.save();
@@ -132,6 +136,115 @@ router.put("/like/:id", auth, async (req, res) => {
   } catch (err) {
     console.error(err.message);
     res.status(500).json("Internal Server Error");
+  }
+});
+
+//@route PUT api/posts/unlike/:id
+//@desc unlike a Post
+//@acess Private
+
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    //Check if the post has already been liked
+
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res
+        .status(400)
+        .json({ msg: "Cannot unlike a post that has not been liked by user" });
+    }
+
+    if (req.user.id === post.user.toString()) {
+      return res.status(400).json({ msg: "Cannot unlike/like your own post" });
+    }
+
+    const removeIndex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id);
+
+    post.likes.splice(removeIndex);
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json("Internal Server Error");
+  }
+});
+
+//@route POST api/posts/comment/:id
+//@desc Create a post
+//@acess Private
+
+router.post(
+  "/comment/:id",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty) {
+      return res.status(404).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const post = await Post.findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+
+      post.comments.unshift(newComment);
+
+      await post.save();
+
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
+//@route DELETE api/posts/comment/:id/:comment_id
+//@desc DELETE a post
+//@acess Private
+
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+
+    if (!comment) {
+      return res.status(404).json({ msg: "Comment does not exist" });
+    }
+
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+
+    const removeIndex = post.comments
+      .map((comment) => comment.user.toString())
+      .indexOf(req.user.id);
+    console.log(removeIndex);
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.stattus(500).send("Internal Server Error");
   }
 });
 
