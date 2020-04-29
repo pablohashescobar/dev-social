0ba8a9ff5e5edf02a54bfc533ba7549c28df9c15
Nikhil <nikhil13012000@gmail.com>
2020-04-29 12:50:23 +0530

    logout

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,8 +2,14 @@
 export const SET_ALERT = "SET_ALERT";
 export const REMOVE_ALERT = "REMOVE_ALERT";
 
-//User Auth
+//User Register
 export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
 export const REGISTER_FAIL = "REGISTER_FAIL";
+// User Authentication
 export const USER_LOADED = "USER_LOADED";
 export const AUTH_ERROR = "AUTH_ERROR";
+// Login
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+//Logout
+export const LOGOUT = "LOGOUT";
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, { useState, Fragment } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { login } from "../../actions/auth";
+import PropTypes from "prop-types";
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
     const [formData, setFormData] = useState({
         email: "",
         password: "",
@@ -15,9 +18,13 @@ const Login = () => {
     const onSubmit = async (e) => {
         e.preventDefault();
 
-        console.log(formData, "Success");
+        login(email, password);
     };
 
+    //Redirect if logged in
+    if (isAuthenticated) {
+        return <Redirect to='/dashboard' />;
+    }
     return (
         <Fragment>
             <h1 className='large text-primary'>Sign In</h1>
@@ -60,4 +67,13 @@ const Login = () => {
     );
 };
 
-export default Login;
+login.propTypes = {
+    login: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => ({
+    isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, { login })(Login);
