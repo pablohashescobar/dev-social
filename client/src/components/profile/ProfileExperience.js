add872e8c46fc2b3c3b45e2b02d0c11fdb5b1bba
Nikhil <nikhil13012000@gmail.com>
2020-05-03 21:48:52 +0530

    profile Education

---
+++ client/src/components/profile/ProfileExperience.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Moment from 'react-moment';
+
+const ProfileExperience = ({
+    experience: { company, title, location, curren, to, from, description },
+}) => {
+    return (
+        <div>
+            <h3 className='text-dark'>{company}</h3>
+            <p>
+                <Moment format='YYYY/MM/DD'>{from}</Moment>
+            </p>{' '}
+            - {!to ? 'Now' : <Moment format='YYYY/MM/DD'>{to}</Moment>}
+            <p>
+                <strong>Position:</strong>
+                {title}
+            </p>
+            <p>
+                <strong>Title:</strong>
+                {description}
+            </p>
+        </div>
+    );
+};
+
+ProfileExperience.propTypes = {
+    experience: PropTypes.array.isRequired,
+};
+
+export default ProfileExperience;
